908d959c9d98a09c6e32590e223eede33209e860
Leo-byno <stiximir100@gmail.com>
2024-12-12 20:59:24 +0100

    Version WEB uniquement possible désormais

--- hangman.go
+++ hangman.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"os/exec"
-	"runtime"
 	"time"
 )
 
@@ -20,19 +18,7 @@ type Data struct { // Structure de données pour le jeu
 	AlreadyUsed []string
 }
 
-func consoleReset() {
-	var cmd *exec.Cmd
-	switch runtime.GOOS {
-	case "windows":
-		cmd = exec.Command("cmd", "/c", "cls")
-	default:
-		cmd = exec.Command("clear")
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
-
-func load(savefile string) {
+func load(savefile string) *Data {
 	var jeu Data
 	game := readWordsFromFile(savefile) // lit le fichier de sauvegarde
 	if len(game) == 0 {
@@ -42,12 +28,7 @@ func load(savefile string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Welcome back, you have %d tries remaining.\n\n", jeu.Life)
-	for i := 0; i < len(jeu.HiddenWord); i++ {
-		fmt.Print(string(jeu.HiddenWord[i]), " ")
-	}
-	fmt.Println()
-	input(&jeu)
+	return &jeu
 }
 
 func save(jeu *Data) {
@@ -61,7 +42,7 @@ func save(jeu *Data) {
 	}
 }
 
-func readWordsFromFile(filename string) []string { // Lecture du fichier, retourne la liste de mot avec words
+func readWordsFromFile(filename string) []string { // Lecture du fichier, retourne la liste de mots avec words
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -80,12 +61,12 @@ func readWordsFromFile(filename string) []string { // Lecture du fichier, retour
 	return words
 }
 
-func HideWordStart(jeu *Data, randWord string) { // Mise en place des variables Word et Hiddenword de la structure jeu
+func HideWordStart(jeu *Data, randWord string) {
 	jeu.Word = randWord // Le mot complet dans jeu.Word
-	hiddenword := ""    // le mot incomplet qui va etre construit
+	hiddenword := ""    // Le mot incomplet qui va être construit
 	rand.Seed(time.Now().UnixNano())
 	var randomletter []int
-	for i := 0; i < len(randWord)/2-1; i++ { // On recherche des index aleatoire du mot complet selon la taille du mot
+	for i := 0; i < len(randWord)/2-1; i++ { // On recherche des index aléatoires du mot complet
 		randomletter = append(randomletter, rand.Intn(len(randWord)))
 		if len(randomletter) > 1 {
 			for n := 0; n < len(randomletter); n++ {
@@ -96,14 +77,14 @@ func HideWordStart(jeu *Data, randWord string) { // Mise en place des variables 
 		}
 	}
 	var randomlettervalue []rune
-	for i, v := range randWord { // On convertis ces index en des lettres pour pouvoir toutes les scanner et les placer dans la prochaine boucle
+	for i, v := range randWord { // On convertit ces index en des lettres pour les afficher plus facilement
 		for n := 0; n < len(randomletter); n++ {
 			if randomletter[n] == i {
 				randomlettervalue = append(randomlettervalue, v)
 			}
 		}
 	}
-	for i, v := range randWord { // Vérification a chaque tour que les lettres de la liste corresponde a la lettre que l'on observe actuellement dans la boucle
+	for _, v := range randWord {
 		reveal := false
 		for _, n := range randomlettervalue {
 			if n == v {
@@ -111,202 +92,88 @@ func HideWordStart(jeu *Data, randWord string) { // Mise en place des variables 
 				break
 			}
 		}
-		if reveal { // Si lettre trouvé
+		if reveal {
 			hiddenword = hiddenword + string(v)
 			jeu.AlreadyUsed = append(jeu.AlreadyUsed, string(v))
-			fmt.Print(string(hiddenword[i]), " ")
-		} else { // Sinon placement du cache
+		} else {
 			hiddenword = hiddenword + "_"
-			fmt.Print(string(hiddenword[i]), " ")
 		}
 	}
 	jeu.HiddenWord = hiddenword // Intégration du mot caché dans les données
-	fmt.Println()
-	input(jeu) // Requete au joueur
 }
 
-func DisplayHangman(jeu *Data) {
+func DisplayHangman(jeu *Data) string {
 	hangman := readWordsFromFile("Data/hangman.txt")
 	var linenumber int = 8
 	linenumber = linenumber * (10 - jeu.Life - 1)
-	fmt.Println()
+	var hangmanDrawing string
 	for i := 0; i < 7; i++ {
 		if linenumber >= 80 || jeu.Life == 10 {
 			break
 		}
-		fmt.Println(hangman[linenumber][0:9])
+		hangmanDrawing += hangman[linenumber][0:9] + "\n"
 		linenumber++
 	}
+	return hangmanDrawing
 }
 
-func lives(jeu *Data) { // Gestion des vies et de la lose
+func lives(jeu *Data) {
 	jeu.Life -= 1
 	if len(jeu.Letter) > 1 && jeu.Letter != jeu.Word && jeu.Life != 1 {
 		jeu.Life -= 1
-		fmt.Printf("You lost 2 lives !\n\n")
-	}
-	if jeu.Life <= 0 {
-		consoleReset()
-		fmt.Println("You lost, the word was", jeu.Word, "\n")
-	} else {
-		fmt.Printf("You have %v lives remaining\n\n", jeu.Life)
-		for i := 0; i < len(jeu.HiddenWord); i++ {
-			fmt.Print(string(jeu.HiddenWord[i]), " ")
-		}
-		fmt.Println()
 	}
 }
 
-func VictoryCheck(jeu *Data) bool { // Gestion de la victoire
-	if jeu.Word == jeu.HiddenWord || jeu.Letter == jeu.Word {
-		return true
-	}
-	return false
+func VictoryCheck(jeu *Data) bool {
+	return jeu.Word == jeu.HiddenWord || jeu.Letter == jeu.Word
 }
 
-func UpdWord(jeu *Data) { // Update le mot a chaque requete du joueur
-	consoleReset()
+func UpdWord(jeu *Data) string {
 	hiddenRunes := []rune(jeu.HiddenWord) // Ajout de chacune de ces valeurs en rune pour des raisons d'écriture de string plus simple
 	wordRunes := []rune(jeu.Word)
 	letterRune := rune(jeu.Letter[0])
 	var success bool = false
-	for i := 0; i < len(jeu.Word); i++ { // Vérification pour une lettre si elle est présente dans le mot, puis remplacement
+	for i := 0; i < len(jeu.Word); i++ { // Vérification pour une lettre si elle est présente dans le mot
 		if wordRunes[i] == letterRune && len(jeu.Letter) == 1 {
 			hiddenRunes[i] = wordRunes[i]
 			success = true
 		}
 	}
 	if success {
-		fmt.Printf("You have found a letter !\n\n")
 	}
-	if !success || (len(jeu.Letter) > 1 && jeu.Letter != jeu.Word) { // Vérification si la vérif de la lettre est pas vrai ou si on vérif un mot et que le mot est faux
+	if !success || (len(jeu.Letter) > 1 && jeu.Letter != jeu.Word) {
 		lives(jeu)
-	} else if !VictoryCheck(jeu) { // Réecriture du mot caché
-		jeu.HiddenWord = string(hiddenRunes)
-		for i := 0; i < len(jeu.HiddenWord); i++ {
-			fmt.Print(string(jeu.HiddenWord[i]), " ")
-		}
-		fmt.Println()
 	}
-	if VictoryCheck(jeu) { // Si on a gagné
-		consoleReset()
-		fmt.Println("Congratulations, you found", jeu.Word, "\n")
-		DisplayHangman(jeu)
-		fmt.Printf("\nYou had %v lives remaining\n", jeu.Life)
-	} else { // Sinon on redemande la une lettre
-		input(jeu)
+	if VictoryCheck(jeu) {
+		return fmt.Sprintf("Congratulations, you found %s! You had %v lives remaining.", jeu.Word, jeu.Life)
 	}
+	jeu.HiddenWord = string(hiddenRunes)
+	return jeu.HiddenWord
 }
 
-func input(jeu *Data) { // Demander au joueur une lettre
-	var letter string
-	var savestr string
-	var minimalize rune
-	DisplayHangman(jeu)
-	jeu.Letter = ""
-	if jeu.Life > 0 {
-		fmt.Println()
-		fmt.Println("Type a letter: ")
-		fmt.Scan(&letter)
-		for i := 0; i < len(jeu.AlreadyUsed); i++ { // Vérification si lettre déja écrite ou présente
-			if jeu.AlreadyUsed[i] == letter {
-				consoleReset()
-				for i := 0; i < len(jeu.HiddenWord); i++ {
-					fmt.Print(string(jeu.HiddenWord[i]), " ")
-				}
-				fmt.Printf("\n\nYou've already written this letter or those letters\n\nYou have %v lives remaining\n", jeu.Life)
-				input(jeu)
-				return
-			}
-		}
-		if letter == "STOP" { // Possibilité de stopper le jeu
-			consoleReset()
-			fmt.Printf("Game stop !\n\nDo you want to save your game ?\nFor yes, print 'y'\nFor no, print anything else\n")
-			fmt.Scan(&savestr)
-			if savestr == "y" {
-				save(jeu)
-				consoleReset()
-				fmt.Printf("Game saved !\n\nTo restart, use '--startWith game.txt' option\n")
-			}
-			os.Exit(0)
-		} else {
-			jeu.AlreadyUsed = append(jeu.AlreadyUsed, letter)
-		}
-		if len(letter) > 1 { // Vérification pour les mots entiers
-			for i := 0; i < len(letter); i++ {
-				if rune(letter[i]) >= 'A' && rune(letter[i]) <= 'Z' {
-					minimalize = rune(letter[i]) + 32
-					jeu.Letter = jeu.Letter + string(minimalize)
-					fmt.Print(jeu.Letter)
-				} else if rune(letter[i]) >= 'a' && rune(letter[i]) <= 'z' {
-					jeu.Letter = jeu.Letter + string(letter[i])
-					fmt.Print(jeu.Letter)
-				} else if rune(letter[i]) < 'a' || rune(letter[i]) > 'z' { // Vérifications que sa soit des lettres
-					consoleReset()
-					fmt.Printf("Error, try again\n\nYou have %v lives remaining\n\n", jeu.Life)
-					for i := 0; i < len(jeu.HiddenWord); i++ {
-						fmt.Print(string(jeu.HiddenWord[i]), " ")
-					}
-					fmt.Println()
-					input(jeu)
-					break
-				}
-			}
-		} else if rune(letter[0]) >= 'A' && rune(letter[0]) <= 'Z' && len(letter) == 1 { // Vérification pour les lettres
-			minimalize = rune(letter[0]) + 32
-			letter = string(minimalize)
-			jeu.Letter = letter
-		} else if rune(letter[0]) >= 'a' && rune(letter[0]) <= 'z' && len(letter) == 1 {
-			jeu.Letter = letter
-		} else if rune(letter[0]) < 'a' || rune(letter[0]) > 'z' && len(letter) == 1 { // Vérifications que sa soit une lettre
-			consoleReset()
-			fmt.Printf("Error, try again\n\nYou have %v lives remaining\n\n", jeu.Life)
-			for i := 0; i < len(jeu.HiddenWord); i++ {
-				fmt.Print(string(jeu.HiddenWord[i]), " ")
-			}
-			fmt.Println()
-			input(jeu)
-		}
-		UpdWord(jeu)
-	}
-}
-
-func RandomWord(filename string) string { // Prendre aléatoirement un mot de la liste choisis en paramètre
+func RandomWord(filename string) string {
 	words := readWordsFromFile(filename)
 	rand.Seed(time.Now().UnixNano())
 	randomWord := words[rand.Intn(len(words))]
 	return randomWord
 }
 
-func GameStart() { // Démarrage du jeu
-	consoleReset()
-	if len(os.Args) == 3 && os.Args[1] == "--startWith" {
-		load(os.Args[2])
-	} else {
-		var jeu Data
-		jeu.Life = 10
-		var filename string
-		var filenameresult string
-		fmt.Printf("Choose your difficulty : \n1 = Easy\n2 = Medium\n3 = Hard\n")
-		fmt.Scanf("%s", &filename)
-		switch filename {
-		case "1":
-			consoleReset()
-			filenameresult = "Data/words.txt"
-
-		case "2":
-			consoleReset()
-			filenameresult = "Data/words2.txt"
-
-		case "3":
-			consoleReset()
-			filenameresult = "Data/words3.txt"
-		default:
-			consoleReset()
-			fmt.Printf("Error, default difficulty = 1\n\n")
-			filenameresult = "Data/words.txt"
-		}
-		fmt.Printf("Good luck, you have 10 attempts, you can stop the game with 'STOP'\n\n")
-		HideWordStart(&jeu, RandomWord(filenameresult))
+func GameStart(level string) *Data {
+	var jeu Data
+	jeu.Life = 10
+	var filename string
+	switch level {
+	case "1":
+		filename = "Data/words.txt"
+	case "2":
+		filename = "Data/words2.txt"
+	case "3":
+		filename = "Data/words3.txt"
+	default:
+		filename = "Data/words.txt"
 	}
+	HideWordStart(&jeu, RandomWord(filename))
+	return &jeu
 }
+
+// le hangman fonctionne uniquement avec la version WEB
